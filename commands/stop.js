14c38044b55058677770e4f8f2aa3dd81d7df91e
LegoDev <lyonga10@dukes.ncea.org.uk>
2020-11-19 23:35:09 +0000

    Prevent users from stopping while more than 1 user is in VC

--- commands/stop.js
+++ commands/stop.js
@@ -9,6 +9,8 @@ module.exports = {
     if (!queue) return message.reply("There is nothing playing.").catch(console.error);
     if (!canModifyQueue(message.member)) return;
 
+    if (queue.connection.channel.members.cache.array().length != 2) return message.reply("Cannot stop music: here are more users in the voice channel.");
+
     queue.songs = [];
     queue.connection.dispatcher.end();
     queue.textChannel.send(`${message.author} ⏹ stopped the music!`).catch(console.error);
